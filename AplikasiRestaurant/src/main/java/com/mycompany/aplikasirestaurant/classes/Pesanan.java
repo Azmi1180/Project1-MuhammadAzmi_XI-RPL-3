1b7cc4b885e2cc8591a0e3332bacec8e2f46083b
Muhammad Azmi <muhammadazmi4456@gmail.com>
2021-02-24 14:44:41 +0700

    Implementasi Kelas Daftar Menu

--- AplikasiRestaurant/src/main/java/com/mycompany/aplikasirestaurant/classes/Pesanan.java
+++ AplikasiRestaurant/src/main/java/com/mycompany/aplikasirestaurant/classes/Pesanan.java
@@ -10,5 +10,14 @@ package com.mycompany.aplikasirestaurant.classes;
  * @author azmi
  */
 public class Pesanan {
+    private Menu menu;
+    private int jumlah;
+    private String keterangan;
+    
+    public Pesanan(Menu menu, int jumlah) { }
+    
+    public void setKeterangan(String keterangan) { }
+    
+    public String getKeterangan() { return ""; } {}
     
 }

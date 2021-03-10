8f30dca71de72ecb228e90ce74f49aceb4b64d82
Muhammad Azmi <muhammadazmi4456@gmail.com>
2021-03-10 10:27:17 +0700

    Modul D: Inisiasi Transaksi

--- AplikasiRestaurant/src/main/java/main/MainAplikasiKasir.java
+++ AplikasiRestaurant/src/main/java/main/MainAplikasiKasir.java
@@ -6,7 +6,12 @@
 package main;
 
 import com.azmi.aplikasirestaurant.classes.DaftarMenu;
+import com.azmi.aplikasirestaurant.classes.Kuah;
+import com.azmi.aplikasirestaurant.classes.Minuman;
 import com.azmi.aplikasirestaurant.classes.Ramen;
+import com.azmi.aplikasirestaurant.classes.Toping;
+
+import java.util.Scanner;
 
 
 /**
@@ -18,7 +23,23 @@ import com.azmi.aplikasirestaurant.classes.Ramen;
 public class MainAplikasiKasir {   
     public DaftarMenu daftarMenu;
     
-    public static void main(String[] args) {}
+    // Tambahkan 
+    public static double PAJAK_PPN = 0.10;
+    public static double BIAYA_SERVICE = 0.05;
+   
+    // End Of Tambahkan
+    public static void main(String[] args) {
+        // init
+        Scanner input = new Scanner(System.in);
+        //Tambahkan
+        String no_transaksi, nama_pemesan, tanggal, no_meja = "";
+        String transaksi_lagi = "", pesan_lagi = "", keterangan = "", makan_ditempat;
+        // End Of Tambahkan
+        
+        MainAplikasiKasir app = new MainAplikasiKasir();
+        app.generateDaftarMenu();
+        
+    }
     
     public void generateDaftarMenu() { 
         daftarMenu = new DaftarMenu();
@@ -26,18 +47,25 @@ public class MainAplikasiKasir {
         daftarMenu.tambahMenu(new Ramen("Ramen Original", 18000));
         daftarMenu.tambahMenu(new Ramen("Ramen Vegetarian", 22000));
         daftarMenu.tambahMenu(new Ramen("Ramen Karnivor", 28000));
-        daftarMenu.tambahMenu(new Kuah("Ramen Orisinil", 25000));
-        daftarMenu.tambahMenu(new Kuah("Ramen Seafood", 25000));
-        daftarMenu.tambahMenu(new Kuah("Ramen Seafood", 25000));
-        daftarMenu.tambahMenu(new Kuah ("Ramen Seafood", 25000));
-        daftarMenu.tambahMenu(new Ramen("Ramen Seafood", 25000));
-        daftarMenu.tambahMenu(new Ramen("Ramen Seafood", 25000));
-        daftarMenu.tambahMenu(new Ramen("Ramen Seafood", 25000));
-        daftarMenu.tambahMenu(new Ramen("Ramen Seafood", 25000));
-        daftarMenu.tambahMenu(new Ramen("Ramen Seafood", 25000));
+        
+        daftarMenu.tambahMenu(new Kuah("Kuah Orisinil"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Internasional"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Spicy Lada"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Soto Padang"));
+
+        daftarMenu.tambahMenu(new Toping("Crab Stick Bakar", 6000));
+        daftarMenu.tambahMenu(new Toping("Chicken Katsu", 8000));
+        daftarMenu.tambahMenu(new Toping("Gyoza Goreng", 4000));
+        daftarMenu.tambahMenu(new Toping("Bakso Goreng", 7000));
+        daftarMenu.tambahMenu(new Toping("Enoki Goreng", 5000));
+
+        daftarMenu.tambahMenu(new Minuman("Jus Alpukat SPC", 10000));
+        daftarMenu.tambahMenu(new Minuman("Jus Stroberi", 11000));
+        daftarMenu.tambahMenu(new Minuman("Capucino Coffee", 15000));
+        daftarMenu.tambahMenu(new Minuman("Vietnam Dripp", 14000));
         
         
-        
+        daftarMenu.tampilDaftarMenu();
     }
 
     
